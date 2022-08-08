24fc8a43fefaf6daea852438230fe0d7cbe42a70
NereuAugusto <nereubeckercampos711@gmail.com>
2022-08-08 12:19:57 -0300

    Primeira aula

---
+++ Aula01.py
@@ -0,0 +1,31 @@
+print(2+3)
+print(4-7)
+print(2*5.3)
+print(9.4/3)
+print(10%3)
+#potencia 
+print(2**5.3)
+#pega o numero que foi usado pra dividir 
+print(10//3)
+#raiz
+print(81 ** (1/2))
+#vazio é diferente de nulo que é diferente de 0
+
+#tipo String 
+dir(str)
+
+nome = 'Andre Insardi'
+print(nome)
+
+print('Andre' == "Andre")
+texto = 'texto entre apostrófos pode ter "aspas"'
+print(texto)
+
+doc= """texto com multiplas linhas..."""
+print(doc)
+
+#pegar letras especificas de um texto
+nome = "Um nome qualquer"
+nome[0]
+nome[6]
+nome[-3]
